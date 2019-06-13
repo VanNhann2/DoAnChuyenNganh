66d1e8adfd667d12304b40d440555cb6864792df
Võ Văn Nhẫn <43567147+VanNhann2@users.noreply.github.com>
2019-06-13 11:50:54 +0700

    Updates

---
+++ precache-manifest.e547fb14a244d1e0494f73aa2d229378.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645c5dd5d8335f1d71ff39d2b0f0b047",
+    "url": "/DoAnChuyenNganh/index.html"
+  },
+  {
+    "revision": "2299ddbfb9a8a8668008",
+    "url": "/DoAnChuyenNganh/static/css/main.af519b88.chunk.css"
+  },
+  {
+    "revision": "0334989a69ba1a0a76ba",
+    "url": "/DoAnChuyenNganh/static/js/2.45e8c35f.chunk.js"
+  },
+  {
+    "revision": "2299ddbfb9a8a8668008",
+    "url": "/DoAnChuyenNganh/static/js/main.d58beb0f.chunk.js"
+  },
+  {
+    "revision": "a915ff09699704988dc5",
+    "url": "/DoAnChuyenNganh/static/js/runtime~main.073693e3.js"
+  },
+  {
+    "revision": "7f7e3b51e730970ced611f4d2d3826b4",
+    "url": "/DoAnChuyenNganh/static/media/8.7f7e3b51.jpg"
+  },
+  {
+    "revision": "9dc7c0bc54ebff524bd4d998d629e57f",
+    "url": "/DoAnChuyenNganh/static/media/t1.9dc7c0bc.jpg"
+  },
+  {
+    "revision": "d66427e516f661117e287fb5bd2a6621",
+    "url": "/DoAnChuyenNganh/static/media/t2.d66427e5.jpg"
+  },
+  {
+    "revision": "d2521813751da5ab3d07954ad4980b79",
+    "url": "/DoAnChuyenNganh/static/media/t3.d2521813.jpg"
+  },
+  {
+    "revision": "61f2a8afaedf009d59ced5ee34790afe",
+    "url": "/DoAnChuyenNganh/static/media/t4.61f2a8af.jpg"
+  }
+]);
